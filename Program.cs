d8d00635c7dafafb7f471db1d371da8d58f5137c
Caleb Walls <caleb.walls@sync1systems.com>
2024-04-16 22:09:47 -0500

    got rid of swagger for dev env

--- Program.cs
+++ Program.cs
@@ -43,9 +43,11 @@ else
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseSwagger();
-app.UseSwaggerUI();
-
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
